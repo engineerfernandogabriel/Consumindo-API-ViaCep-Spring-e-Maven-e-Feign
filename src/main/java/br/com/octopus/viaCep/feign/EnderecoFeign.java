3726ed9b0e355b079464d245f66a7b60ecf8505a
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-07-04 09:47:25 -0300

    Create: search cep for adreess

--- src/main/java/br/com/octopus/viaCep/feign/EnderecoFeign.java
+++ src/main/java/br/com/octopus/viaCep/feign/EnderecoFeign.java
@@ -5,9 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface EnderecoFeign {
 
     @GetMapping("{cep}/json")
-    EnderecoResponse buscarEnderecoCep(@PathVariable("cep") String cep);
+    EnderecoResponse buscarEnderecoPeloCep(@PathVariable("cep") String cep);
+
+    @GetMapping("{uf}/{cidade}/{nomeDaRua}/json")
+    List<EnderecoResponse> buscarCepPeloEndereco(@PathVariable("uf") String uf, @PathVariable("cidade") String cidade, @PathVariable("nomeDaRua") String nomeDaRua);
 }
--- src/main/java/br/com/octopus/viaCep/service/EnderecoService.java
+++ src/main/java/br/com/octopus/viaCep/service/EnderecoService.java
@@ -1,12 +1,15 @@
 package br.com.octopus.viaCep.service;
 
 import br.com.octopus.viaCep.exception.CepInvalidoException;
-import br.com.octopus.viaCep.exception.CepNaoEncontradoException;
+import br.com.octopus.viaCep.exception.CidadeEnderecoInvalidoException;
 import br.com.octopus.viaCep.feign.EnderecoFeign;
 import br.com.octopus.viaCep.model.EnderecoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,7 +17,7 @@ public class EnderecoService {
 
     private final EnderecoFeign feign;
 
-    public ResponseEntity<?> consultarCep(String cep) {
+    public ResponseEntity<?> consultaEndereco(String cep) {
         cep = cep.replaceAll("\\D", "");
 
         try{
@@ -22,16 +25,50 @@ public class EnderecoService {
                 throw new CepInvalidoException("Cep inválido: o número do cep deve conter 8 dígitos");
             }
 
-            EnderecoResponse response = feign.buscarEnderecoCep(cep);
+            EnderecoResponse response = feign.buscarEnderecoPeloCep(cep);
 
             if(response.cep() == null) {
-                throw new CepNaoEncontradoException("Cep não encontrado");
+                return ResponseEntity.ok("Cep não encontrado");
             }
 
             return ResponseEntity.ok(response);
 
-        } catch (CepInvalidoException | CepNaoEncontradoException e) {
+        } catch (CepInvalidoException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    public ResponseEntity<?> consultaCep(String uf, String cidade, String nomeDaRua) {
+        cidade = formatterString(cidade);
+        nomeDaRua = formatterString(nomeDaRua);
+
+        try {
+            if(cidade.length() < 3) {
+                throw new CidadeEnderecoInvalidoException("Cidade inválida: o nome da cidade deve conter no mínimo 3 caracteres");
+            }
+
+            if(nomeDaRua.length() < 3) {
+                throw new CidadeEnderecoInvalidoException("Logradouro inválido: o nome da rua (logradouro) deve conter no mínimo 3 caracteres");
+            }
+
+            List<EnderecoResponse> response = feign.buscarCepPeloEndereco(uf, cidade, nomeDaRua);
+
+            if (response.isEmpty()) {
+                return ResponseEntity.ok("Endereço não encontrado");
+            }
+            return ResponseEntity.ok(response);
+
+        } catch (CidadeEnderecoInvalidoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public String formatterString (String word) {
+        return Normalizer.normalize(word, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
+                .replaceAll("[^\\w\\s]","")
+                .replaceAll("\\s+", " ")
+                .trim();
+    }
+
 }
