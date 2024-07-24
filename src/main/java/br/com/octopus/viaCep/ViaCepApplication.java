787f7de87b5847752057fce1a47a1d0bbad5afdd
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-07-24 11:37:36 -0300

    Create: Swagger Config

--- src/main/java/br/com/octopus/viaCep/ViaCepApplication.java
+++ src/main/java/br/com/octopus/viaCep/ViaCepApplication.java
@@ -1,11 +1,14 @@
 package br.com.octopus.viaCep;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ViaCep API with Feign-Maven", version = "1.0", description = "API para consulta de CEP"))
 public class ViaCepApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/octopus/viaCep/controller/EnderecoController.java
+++ src/main/java/br/com/octopus/viaCep/controller/EnderecoController.java
@@ -1,6 +1,10 @@
 package br.com.octopus.viaCep.controller;
 
 import br.com.octopus.viaCep.service.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,15 +12,26 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/cep")
 @RestController
+@Tag(name = "endereco controller")
 public class EnderecoController {
 
     private final EnderecoService service;
 
+    @Operation(summary = "consultar endereço pelo cep", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Consulta Ok"),
+        @ApiResponse(responseCode = "400", description = "Parâmetros Inválidos"),
+    })
     @GetMapping("/consultaEndereco/{cep}")
     public ResponseEntity<?> consultaEndereco(@PathVariable String cep) {
         return service.consultaEndereco(cep);
     }
 
+    @Operation(summary = "consultar cep pelo endereço", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Consulta Ok"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros Inválidos"),
+    })
     @GetMapping("/consultaCep")
     public ResponseEntity<?> consultaCep(@RequestParam String uf, @RequestParam String cidade, @RequestParam String nomeDaRua){
         return service.consultaCep(uf, cidade, nomeDaRua);
